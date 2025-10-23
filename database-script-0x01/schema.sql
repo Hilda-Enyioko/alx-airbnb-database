4c582fc3af9d360d07f80534107b7a39d4461c22
Hilda Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2025-10-23 16:34:14 +0100

    Update schema.sql

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,1 +1,69 @@
+CREATE DATABASE airbnb;
 
+USE airbnb;
+
+CREATE TABLE user (
+    user_id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(210) NOT NULL,
+    password_hash VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(14) NOT NULL,
+    role ENUM ('guest', 'host', 'admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE property (
+    property_id INT PRIMARY KEY NOT NULL,
+    host_id INT FOREIGN KEY NOT NULL,
+    name VARCHAR(132) NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(132) NOT NULL,
+    pricepernight DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (host_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE booking (
+    booking_id INT PRIMARY KEY NOT NULL,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    status ENUM ('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES property(property_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE payment (
+    payment_id INT PRIMARY KEY NOT NULL,
+    booking_id INT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM ('credit_card', 'paypal', 'stripe') NOT NULL,
+    FOREIGN KEY (booking_id) REFERENCES booking(booking_id)
+);
+
+CREATE TABLE review (
+    review_id INT PRIMARY KEY NOT NULL,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES property(property_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE message (
+    message_id INT PRIMARY KEY NOT NULL,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    content TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES user(user_id),
+    FOREIGN KEY (receiver_id) REFERENCES user(user_id)
+);
